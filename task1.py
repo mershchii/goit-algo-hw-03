bf59d19e480239defdd94c09eeba3630267ad44d
mershchii <mershchi@gmail.com>
2026-07-16 12:15:28 +0300

    Виправив логіку для граничних та не валідних значень параметрів

--- task1.py
+++ task1.py
@@ -6,19 +6,16 @@ from datetime import datetime
 #   - Ф-ція перетіорює дату на кількість днів (ігнорує години, хвилини, секунди)
 #   - Рахує різницю між датою вводу та сьогодіншньою датою та повертає результат
 
-def get_days_from_today(date: str) -> int:    # Тепер в ф=цію date є str та повертає int
-    days_from_today = datetime.today().date().toordinal() - date.toordinal()
-    return days_from_today                    # <-- Виправлено ф-цію, тепер повертає результат
+date = "d2025-02-14"
 
+def get_days_from_today(date: str) -> int:   
 
-# Перевіряємо корекність дати 
-while True:
-    try:                                # Просимо ввести дату, зразу ж перетворюючи її в об'єкт datetime 
-                                        # При некоректній даті, цикл повідомляє "Дата некореткна" та повторно просить ввести дату
-        date = datetime.strptime(input("Введіть дату в форматі 'РРРР-ММ-ДД': "), "%Y-%m-%d").date()
-        break                           # завершуємо цикл while
-    except ValueError:                  
-        print("Дата некореткна")
-
-result = get_days_from_today(date)               # Застосовуємо ф-цію 
-print(result)                                    # виводимо результат в термінал для зручності 
+    while True:
+        try:                                
+            date_datetime = datetime.strptime(date, "%Y-%m-%d").date().toordinal()
+            return datetime.today().date().toordinal() - date_datetime
+        except ValueError:                  
+            print("Дата некореткна")
+            break
+    
+print(get_days_from_today(date))                                    
--- task2.py
+++ task2.py
@@ -3,21 +3,21 @@ import random
 #    Функція генерує вказану кількість унікальних чисел у заданому діапазоні.
 #    Функція повертає список випадково вибраних, відсортованих чисел. Числа в наборі не повинні повторюватися. Якщо параметри не відповідають заданим обмеженням, функція повертає пустий список.
 
-min = random.randint(2, 1000)                # Генеруємо випадкове число min
-max = random.randint(2, 1000)                # Генеруємо випадкове число max
-quantity = random.randint(1, 10)             # Генеруємо випадкове число quantity 
+min = random.randint(1, 1000)                # Генеруємо випадкове число min
+max = random.randint(1, 1000)                # Генеруємо випадкове число max
+quantity = random.randint(1, 10)             # Генеруємо випадкове число quantity
 
 def get_numbers_ticket(min, max, quantity):
 
     numbers = set()                          # Задаємо пусту множину 
 
 
-    if min < 1 or max > 1000 or min >= max:  # перевіряємо значення 
+    if min < 1 or max > 1000 or min >= max or min > 1000 or max < 1 or max-min < quantity:  # перевіряємо значення 
         print("Некоректні дані")             # якщо дані невалідні, повертається пустий список
         return numbers                       
     else:                                    # Якщо валідні, повертається множина з відсортованими та унікольними числами 
         while len(numbers) < quantity:   
-            num = random.randint(min, max+1)
+            num = random.randint(min, max)
             numbers.add(num)
         return sorted(numbers)
 
